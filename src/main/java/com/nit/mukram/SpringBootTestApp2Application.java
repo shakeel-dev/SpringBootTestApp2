b04a799bfe284bb867a7bfacdaa29e09a574d0bf
admin123 <admin123@DESKTOP-MN6M8PH>
2020-07-26 01:41:56 -0700

    MMG-786- First Commit Done

---
+++ src/main/java/com/nit/mukram/SpringBootTestApp2Application.java
@@ -0,0 +1,13 @@
+package com.nit.mukram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nit/mukram/SpringBootTestApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit.mukram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
